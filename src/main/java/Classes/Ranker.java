a2a9c7feb8ed7aaea0be834c65f5561250aa6f8a
AliTolba <Ali_Ahmed97@hotmail.com>
2018-05-01 15:08:29 +0200

    rankerOld

--- src/main/java/Classes/Ranker.java
+++ src/main/java/Classes/Ranker.java
@@ -8,14 +8,19 @@ import java.util.HashMap;
 public class Ranker {
 
     private DBConnection db;
-
+    private double DampingFactor;
     public static void main(String[] args) {
 
+        new Ranker();
 
     }
 
     Ranker(){
         this.db = DBConnection.getInstance();
+        for(int j=0;j<100;j++)
+        {
+            this.pageRanker();
+        }
     }
     
     private void pageRanker() {
@@ -23,11 +28,25 @@ public class Ranker {
         HashMap<String, Document> arr = db.getDocumentsByFilter(new Document(), DBConnection.FETCHED_URLs);
 
         for(HashMap.Entry<String, Document> el : arr.entrySet()){
-            el.getKey();
+            String URL=el.getKey();
 
             @SuppressWarnings("unchecked")
-            ArrayList<String> aaa=  el.getValue().get("inLinks", ArrayList.class);
+            ArrayList<String> inLinks=  el.getValue().get("inLinks", ArrayList.class);
+
+            double PageRank=(1-this.DampingFactor);
+            for(int i=0;i<inLinks.size();i++)
+            {
+                PageRank+=this.DampingFactor*(arr.get(inLinks.get(i)).getDouble("rank")/arr.get(inLinks.get(i)).getDouble("outLinks"));
+            }
+
+            el.getValue().put("rank",PageRank);
+
         }
 
+    }
+
+    private void tfidf()
+    {
+        
     }
 }
